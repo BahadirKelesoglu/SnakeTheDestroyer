5bdbd5897bcd81cab41f37f02cb14a402d73cf84
Bahadır Keleşoğlu <bahadirkelesoglu@ogrenci.karabuk.edu.tr>
2023-09-16 13:40:54 +0300

    Heading to GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager instance;
+    public static GameManager Instance { get { return instance; } }
+    public int coin = 0, numberOfSkins = 0, activeSkin = 0;
+
+    private void Awake()
+    {
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+        if (PlayerPrefs.HasKey("numberOfSkins"))
+        {
+            coin = PlayerPrefs.GetInt("coin");
+            numberOfSkins = PlayerPrefs.GetInt("numberOfSkins");
+            activeSkin = PlayerPrefs.GetInt("activeSkin");
+        }
+        else
+            Save();
+    }
+
+    public void Save()
+    {
+        PlayerPrefs.SetInt("coin", coin);
+        PlayerPrefs.SetInt("numberOfSkins", numberOfSkins);
+        PlayerPrefs.SetInt("activeSkin", activeSkin);
+    }
+}
--- Assets/Scripts/menuManage.cs
+++ Assets/Scripts/menuManage.cs
@@ -1,20 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.Tracing;
 using System.Security.Claims;
+using TMPro;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.U2D;
 using UnityEngine.UI;
 
 public class menuManage : MonoBehaviour
 {
-    [SerializeField] GameObject LButton, LPanelChild, LPanel,MPanel;
+    private int coins;
+    public TMP_Text coinUI;
+    [SerializeField] GameObject LButton, LPanelChild, LPanel,MPanel, MPanelChild, MButton, snakeGun,buyButton;
+    private Sprite emptySprite;
     // Start is called before the first frame update
 
     private void Awake()
     {
         MPanel.SetActive(false);
         LPanel.SetActive(false);
+        buyButton.SetActive(false);
+        coinUI.text = "Coins: " + coins.ToString();
+        emptySprite = snakeGun.GetComponent<SpriteRenderer>().sprite;
+        
     }
     void Start()
     {
@@ -28,6 +38,18 @@ public class menuManage : MonoBehaviour
             string sceneName = sprite.name;
             temp.GetComponent<Button>().onClick.AddListener(() => LoadLevel(sceneName));
         }
+
+        Sprite[] spritesPlayer = Resources.LoadAll<Sprite>("Player");
+
+        foreach (Sprite sprite in spritesPlayer)
+        {
+            GameObject temp = Instantiate(MButton) as GameObject;
+            temp.GetComponent<Image>().sprite = sprite;
+            temp.transform.SetParent(MPanelChild.transform, false);
+            temp.GetComponent<Button>().onClick.AddListener(() => selectGun(sprite));
+
+        }
+        //snakeGun.GetComponent<SpriteRenderer>().sprite = spritesPlayer[0];
     }
 
     // Update is called once per frame
@@ -49,5 +71,28 @@ public class menuManage : MonoBehaviour
     public void OpenMarketPanel()
     {
         MPanel.SetActive(!MPanel.activeSelf);
+    }
+
+    public void addCoin(int value) {
+
+        coins += value;
+        coinUI.text = "Coins: " + coins.ToString();
+
+    }
+
+    public void selectGun(Sprite sprite)
+    {
+        if (snakeGun.GetComponent<SpriteRenderer>().sprite != sprite) { 
+            snakeGun.GetComponent<SpriteRenderer>().sprite = sprite;
+            buyButton.SetActive(true);
+        }
+        else
+        {
+            snakeGun.GetComponent<SpriteRenderer>().sprite = emptySprite;
+            buyButton.SetActive(false);
+        }
+     
+        
+        
     }
 }
